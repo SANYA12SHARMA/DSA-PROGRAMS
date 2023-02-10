22520f64ced7ea976f9faa879faa21eb5a118662
SANYA12SHARMA <97424464+SANYA12SHARMA@users.noreply.github.com>
2023-02-10 11:08:27 +0530

    Update Pattern2.cpp

--- Patterns/Pattern2.cpp
+++ Patterns/Pattern2.cpp
@@ -4,20 +4,25 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=n;i>=1;i--){
-        char x='A';
-        for(int j=1;j<=i;j++){
-            cout<<x;
-            x++;
+    for(int i=0;i<n;i++){
+        int k=1;
+        for(int j=n-1;j>=i;j--){
+            cout<<k;
+            k++;
         }
-        
+        for(int star=0;star<(2*i);star++){
+            cout<<'*';
+        }
+        for(int x=n-i;x>0;x--){
+            cout<<x;
+        }
         cout<<endl;
     }
     return 0;
 }
-// 5
-// ABCDE
-// ABCD
-// ABC
-// AB
-// A
+
+// 4
+// 12344321
+// 123**321
+// 12****21
+// 1******1
