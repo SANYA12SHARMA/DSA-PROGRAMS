cd07cdb783ae071547f9a8b745e71b033821ae74
SANYA12SHARMA <97424464+SANYA12SHARMA@users.noreply.github.com>
2023-07-10 19:47:06 +0530

    Update Reverse.java

--- Arrays/Reverse.java
+++ Arrays/Reverse.java
@@ -1,1 +1,23 @@
-
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        char[] arr=str.toCharArray();
+        if(arr.length==0){
+            return str;
+        }
+        int s=0;
+        int e=arr.length-1;
+        while(s<=e){
+            char t=arr[s];
+            arr[s]=arr[e];
+            arr[e]=t;
+            s++;
+            e--;
+        }
+        String str1=new String(arr);
+        return str1;
+    }
+}
